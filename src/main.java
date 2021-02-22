b6a259b5000e5a7653dc5c887d4fedab34c952de
Allun <allan.backman@gmail.com>
2021-02-22 11:09:07 +0100

    feat - mysql library

--- src/main.java
+++ src/main.java
@@ -1,7 +1,39 @@
 import javax.swing.*;
+import java.sql.*;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+            // Set up connection to database
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://" + "localhost" + ":" + "3306" + "/" + "SAD" +
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    "Allun", JOptionPane.showInputDialog(null, "Password"));
+
+            // Variables
+
+            Statement stmt = conn.createStatement();
+            Scanner scanner = new Scanner(System.in);
+            int activeRoom = 1;
+
+            //   Variables   //
+
+
+
+            //  Main Loop
+
+            while(activeRoom != 0) {
+                String strSelect = "select body from story where id = " + activeRoom;
+                ResultSet results = stmt.executeQuery(strSelect);
+                results.next();
+                System.out.println(results.getString("body"));
+            }
+
+            //  Main Loop   //
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
